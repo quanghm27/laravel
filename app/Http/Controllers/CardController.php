55b6bbd307041cd31c2a6687dee4aa682e6201c0
Hoang Minh Quang (FSU17.R73) <QuangHM1@fsoft.com.vn>
2017-05-18 18:30:11 +0700

    update method delete card

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -191,9 +191,13 @@ class CardController extends Controller {
 	 * @param int $id        	
 	 * @return \Illuminate\Http\Response
 	 */
-	public function destroy($id) {
+	public function destroy(Request $request) {
 		//
-		$card = Card::findOrFail ( $id );
+		
+		$cardId = $request->cardId;
+		
+		
+		$card = Card::findOrFail ( $cardId );
 		
 		if ($card != null) {
 			$data = array (
